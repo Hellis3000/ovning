39c5fcdbd1fba8c230f312fa1d32292a9876e6be
Erik <e.lagnefeldt@gmail.com>
2023-09-12 16:52:47 +0200

    ovning

--- ovning/Program.cs
+++ ovning/Program.cs
@@ -1,2 +1,62 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello,Test!");
+using System.ComponentModel;
+using System.Data.Common;
+
+namespace Ovning1
+{
+
+    public class Program
+    {
+
+        private string empName;
+        private string empPay;
+
+        public void GetItemData()
+        {
+            ShowList();
+        }
+
+        private void ReadName()
+        {
+            Console.WriteLine("Enter Name: ");
+            empName = Console.ReadLine();
+        }
+
+        private void ReadPay()
+        {
+            Console.WriteLine("Enter Pay: ");
+            empPay = int.Parse(Console.ReadLine());
+        }
+
+        private void UpdateList()
+        {
+            int[] pay = [10];
+            for (int i = 0; i < pay.Length; i++)
+            {
+                pay[i] = empPay;
+
+            }
+
+            string[] name = [""];
+            for (int i = 0; i < pay.Length; i++)
+            {
+                name[i] = empName;
+
+            }
+
+
+
+
+      
+
+
+        }
+
+        private void ShowList()
+        {
+            Console.WriteLine(name[i], pay[i]);
+        }
+
+    }
+
+}
